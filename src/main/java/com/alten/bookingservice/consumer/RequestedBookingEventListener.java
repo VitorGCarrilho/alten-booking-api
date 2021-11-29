d223dc444d7804e8b3dfe63d70623c46091a937e
Vitor Carrilho <vitorg.carrilho@gmail.com>
2021-11-29 02:01:05 +0000

    Retry and send to dlq

--- src/main/java/com/alten/bookingservice/consumer/RequestedBookingEventListener.java
+++ src/main/java/com/alten/bookingservice/consumer/RequestedBookingEventListener.java
@@ -5,7 +5,13 @@ import com.alten.bookingservice.service.BookingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +22,11 @@ public class RequestedBookingEventListener {
     @Autowired
     private BookingService bookingService;
 
+    @RetryableTopic(
+            attempts = "10",
+            backoff = @Backoff(delay = 1000, multiplier = 2.0),
+            autoCreateTopics = "false",
+            topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE)
     @KafkaListener(topics = "${topic.booking.request.name}", groupId = "${consumer.booking.service.group-id}")
     public void listen(Booking booking) {
         logger.info("method=listen message={}", booking);
